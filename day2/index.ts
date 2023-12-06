74f22863df3a1460619f8506c5b191336513476d
nymeku <arnaud.andrieu@epitech.eu>
2023-12-06 12:48:23 +0100

    [+] day2 🐩

--- day2/index.ts
+++ day2/index.ts
@@ -3,16 +3,32 @@ import fs from "fs"
 function main() {
     const rawInput = fs.readFileSync("./input.txt", "utf8")
     const input = rawInput.split(/\r?\n/)
+    let tot = 0
+    input.map((val, i) => {
+        const line = val.split(": ")
+        tot += consumer(line[1])
+    })
+    return tot
 }
 
 function consumer(line: string) {
+    console.log({ line })
     // your code here
+    const code: Record<string, number> = { "red": 0, "green": 0, "blue": 0 }
+    line.split("; ").map((sublist) => {
+        sublist.split(", ").map((val) => {
+            const split = val.split(" ")
+            const nb = parseInt(split[0])
+            if (nb > code[split[1]]) {
+                code[split[1]] = nb
+            }
+        })
+    })
+    return code["red"] * code["blue"] * code["green"]
 }
+//"Game 100: 15 blue, 6 red; 1 green, 2 red; 12 blue, 8 green, 1 red; 1 red, 7 blue"
 
-// console.log(main())
-
-
-
+console.log(main())
 
 
 
@@ -33,8 +49,12 @@ function consumer(line: string) {
 
 
 
-const mocks: Record<string, number> = {
+
+
+
+const mocks: Record<string, boolean> = {
     // tests here
+    // "Game 100: 15 blue, 6 red; 1 green, 2 red; 12 blue, 8 green, 1 red; 1 red, 7 blue": false
 }
 
 Object.keys(mocks).map(m => {
